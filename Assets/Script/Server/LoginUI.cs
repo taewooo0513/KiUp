3cc77d9e13446c109a746d0eab8d50e6191acc62
frod2793 <tmdwhd0344@naver.com>
2020-11-13 02:22:12 +0900

    로그인창및 게스트

---
+++ Assets/Script/Server/LoginUI.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Battlehub.Dispatcher;
+using UnityEngine.SceneManagement;
+using BackEnd;
+using TheBackend;
+public class LoginUI : MonoBehaviour
+{
+    private static LoginUI instance;    // 인스턴스
+
+    public GameObject mainTitle;
+    public GameObject subTitle;
+    public GameObject touchStart;
+    public GameObject loginObject;
+    public GameObject customLoginObject;
+    public GameObject signUpObject;
+    public GameObject errorObject;
+    public GameObject nicknameObject;
+
+    private InputField[] loginField;
+    private InputField[] signUpField;
+    private InputField nicknameField;
+    private Text errorText;
+    private GameObject loadingObject;
+   // private FadeAnimation fadeObject;
+
+    private const byte ID_INDEX = 0;
+    private const byte PW_INDEX = 1;
+    private const string VERSION_STR = "Ver {0}";
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public static LoginUI GetInstance()
+    {
+        if (instance == null)
+        {
+            Debug.LogError("LoginUI 인스턴스가 존재하지 않습니다.");
+            return null;
+        }
+        return instance;
+    }
+
+    void Start()
+    {
+       // mainTitle.SetActive(true);
+    //    touchStart.SetActive(true);
+       // subTitle.SetActive(false);
+        loginObject.SetActive(false);
+        customLoginObject.SetActive(false);
+        signUpObject.SetActive(false);
+        errorObject.SetActive(false);
+        nicknameObject.SetActive(false);
+
+        loginField = customLoginObject.GetComponentsInChildren<InputField>();
+        signUpField = signUpObject.GetComponentsInChildren<InputField>();
+        nicknameField = nicknameObject.GetComponentInChildren<InputField>();
+        errorText = errorObject.GetComponentInChildren<Text>();
+
+       loadingObject = GameObject.FindGameObjectWithTag("Loading");
+        loadingObject.SetActive(false);
+
+      //  var fade = GameObject.FindGameObjectWithTag("Fade");
+     //   if (fade != null)
+        //{
+           // fadeObject = fade.GetComponent<FadeAnimation>();
+       // }
+
+      //  mainTitle.GetComponentInChildren<Text>().text = string.Format(VERSION_STR, Application.version);
+
+   
+    }
+
+    public void TouchStart()
+    {
+        loadingObject.SetActive(true);
+        serverManager.GetInstance().BackendTokenLogin((bool result, string error) =>
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                if (result)
+                {
+                 //   ChangeLobbyScene();
+                    return;
+                }
+                //loadingObject.SetActive(false);
+                if (!error.Equals(string.Empty))
+                {
+                    errorText.text = "유저 정보 불러오기 실패\n\n" + error;
+                    errorObject.SetActive(true);
+                    return;
+                }
+                mainTitle.SetActive(false);
+                touchStart.SetActive(false);
+                subTitle.SetActive(true);
+                customLoginObject.SetActive(true);
+                loginObject.SetActive(true);
+            });
+        });
+
+    }
+
+    public void Login()
+    {
+        if (errorObject.activeSelf)
+        {
+            return;
+        }
+        string id = loginField[ID_INDEX].text;
+        string pw = loginField[PW_INDEX].text;
+
+        if (id.Equals(string.Empty) || pw.Equals(string.Empty))
+        {
+            errorText.text = "ID 혹은 PW 를 먼저 입력해주세요.";
+            errorObject.SetActive(true);
+            return;
+        }
+
+       // loadingObject.SetActive(true);
+        serverManager.GetInstance().CustomLogin(id, pw, (bool result, string error) =>
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                if (!result)
+                {
+                    //loadingObject.SetActive(false);
+                    errorText.text = "로그인 에러\n\n" + error;
+                    errorObject.SetActive(true);
+                    return;
+                }
+                SceneManager.LoadScene("Sample Scene");
+            });
+        });
+    }
+
+    public void SignUp()
+    {
+        if (errorObject.activeSelf)
+        {
+            return;
+        }
+        string id = signUpField[ID_INDEX].text;
+        string pw = signUpField[PW_INDEX].text;
+
+        if (id.Equals(string.Empty) || pw.Equals(string.Empty))
+        {
+            errorText.text = "ID 혹은 PW 를 먼저 입력해주세요.";
+            errorObject.SetActive(true);
+            return;
+        }
+
+      // loadingObject.SetActive(true);
+        serverManager.GetInstance().CustomSignIn(id, pw, (bool result, string error) =>
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                if (!result)
+                {
+                    loadingObject.SetActive(false);
+                    errorText.text = "회원가입 에러\n\n" + error;
+                    errorObject.SetActive(true);
+                    return;
+                }
+                SceneManager.LoadScene("Sample Scene");
+            });
+        });
+    }
+
+    public void ActiveNickNameObject()
+    {
+        Dispatcher.Current.BeginInvoke(() =>
+        {
+            mainTitle.SetActive(false);
+            touchStart.SetActive(false);
+            subTitle.SetActive(true);
+            loginObject.SetActive(false);
+            customLoginObject.SetActive(false);
+            signUpObject.SetActive(false);
+            errorObject.SetActive(false);
+            loadingObject.SetActive(false);
+            nicknameObject.SetActive(true);
+        });
+    }
+
+    public void UpdateNickName()
+    {
+        if (errorObject.activeSelf)
+        {
+            return;
+        }
+        string nickname = nicknameField.text;
+        if (nickname.Equals(string.Empty))
+        {
+            errorText.text = "닉네임을 먼저 입력해주세요";
+            errorObject.SetActive(true);
+            return;
+        }
+     //  loadingObject.SetActive(true);
+        serverManager.GetInstance().UpdateNickname(nickname, (bool result, string error) =>
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                if (!result)
+                {
+                    loadingObject.SetActive(false);
+                    errorText.text = "닉네임 생성 오류\n\n" + error;
+                    errorObject.SetActive(true);
+                    return;
+                }
+               // ChangeLobbyScene();
+            });
+        });
+    }
+
+
+
+    
+
+    public void GuestLogin()
+    {
+        if (errorObject.activeSelf)
+        {
+            return;
+        }
+
+     //   loadingObject.SetActive(true);
+        serverManager.GetInstance().GuestLogin((bool result, string error) =>
+        {
+            Dispatcher.Current.BeginInvoke(() =>
+            {
+                if (!result)
+                {
+                    loadingObject.SetActive(false);
+                    errorText.text = "로그인 에러\n\n" + error;
+                    errorObject.SetActive(true);
+                    return;
+                }
+                SceneManager.LoadScene("Sample Scene");
+            });
+        });
+    }
+
+    //void ChangeLobbyScene()
+    //{
+    //    if (fadeObject != null)
+    //    {
+    //        GameManager.GetInstance().ChangeState(GameManager.GameState.MatchLobby, (bool isDone) =>
+    //        {
+    //            Dispatcher.Current.BeginInvoke(() => loadingObject.transform.Rotate(0, 0, -10));
+    //            if (isDone)
+    //            {
+    //                fadeObject.ProcessFadeOut();
+    //            }
+    //        });
+    //    }
+    //    else
+    //    {
+    //    //    GameManager.GetInstance().ChangeState(GameManager.GameState.MatchLobby);
+    //    }
+    //}
+}
---
+++ Assets/Script/Server/serverManager.cs
@@ -0,0 +1,332 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using static BackEnd.SendQueue;
+using UnityEngine.SocialPlatforms;
+using System;
+
+public class serverManager : MonoBehaviour
+{
+    private static serverManager instance;   // 인스턴스
+    public bool isLogin { get; private set; }   // 로그인 여부
+
+    private string tempNickName;                        // 설정할 닉네임 (id와 동일)
+    public string myNickName { get; private set; } = string.Empty;  // 로그인한 계정의 닉네임
+    public string myIndate { get; private set; } = string.Empty;    // 로그인한 계정의 inDate
+    private Action<bool, string> loginSuccessFunc = null;
+
+    private const string BackendError = "statusCode : {0}\nErrorCode : {1}\nMessage : {2}";
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        instance = this;
+        // 모든 씬에서 유지
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public static serverManager GetInstance()
+    {
+        if (instance == null)
+        {
+            Debug.LogError("BackEndServerManager 인스턴스가 존재하지 않습니다.");
+            return null;
+        }
+
+        return instance;
+    }
+    void Start()
+    {
+        isLogin = false;
+        try
+        {
+            Backend.Initialize(() =>
+            {
+                if (Backend.IsInitialized)
+                {
+
+                    // 비동기 함수 큐 초기화
+                    StartSendQueue(true);
+                }
+                else
+                {
+                    Debug.Log("뒤끝 초기화 실패");
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            Debug.Log("[예외]뒤끝 초기화 실패\n" + e.ToString());
+        }
+    }
+
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+        StopSendQueue();
+    }
+
+    // Update is called once per frame
+    // 뒤끝 토큰으로 로그인
+    public void BackendTokenLogin(Action<bool, string> func)
+    {
+        Enqueue(Backend.BMember.LoginWithTheBackendToken, callback =>
+        {
+            if (callback.IsSuccess())
+            {
+                Debug.Log("토큰 로그인 성공");
+                loginSuccessFunc = func;
+
+                OnPrevBackendAuthorized();
+                return;
+            }
+
+            Debug.Log("토큰 로그인 실패\n" + callback.ToString());
+            func(false, string.Empty);
+        });
+    }
+
+    void OnApplicationPause(bool isPause)
+    {
+        Debug.Log("OnApplicationPause : " + isPause);
+        if (isPause == false)
+        {
+            ResumeSendQueue();
+        }
+        else
+        {
+            PauseSendQueue();
+        }
+    }
+    public void CustomLogin(string id, string pw, Action<bool, string> func)
+    {
+        Enqueue(Backend.BMember.CustomLogin, id, pw, callback =>
+        {
+            if (callback.IsSuccess())
+            {
+                Debug.Log("커스텀 로그인 성공");
+                loginSuccessFunc = func;
+
+                OnPrevBackendAuthorized();
+                return;
+            }
+
+            Debug.Log("커스텀 로그인 실패\n" + callback);
+            func(false, string.Format(BackendError,
+                callback.GetStatusCode(), callback.GetErrorCode(), callback.GetMessage()));
+        });
+    }
+    public void CustomSignIn(string id, string pw, Action<bool, string> func)
+    {
+        tempNickName = id;
+        Enqueue(Backend.BMember.CustomSignUp, id, pw, callback =>
+        {
+            if (callback.IsSuccess())
+            {
+                Debug.Log("커스텀 회원가입 성공");
+                loginSuccessFunc = func;
+
+                OnPrevBackendAuthorized();
+                return;
+            }
+
+            Debug.LogError("커스텀 회원가입 실패\n" + callback.ToString());
+            func(false, string.Format(BackendError,
+                callback.GetStatusCode(), callback.GetErrorCode(), callback.GetMessage()));
+        });
+    }
+    public void UpdateNickname(string nickname, Action<bool, string> func)
+    {
+        Enqueue(Backend.BMember.UpdateNickname, nickname, bro =>
+        {
+            // 닉네임이 없으면 매치서버 접속이 안됨
+            if (!bro.IsSuccess())
+            {
+                Debug.LogError("닉네임 생성 실패\n" + bro.ToString());
+                func(false, string.Format(BackendError,
+                    bro.GetStatusCode(), bro.GetErrorCode(), bro.GetMessage()));
+                return;
+            }
+            loginSuccessFunc = func;
+            OnBackendAuthorized();
+        });
+    }
+    private void OnPrevBackendAuthorized()
+    {
+        isLogin = true;
+
+        OnBackendAuthorized();
+    }
+    private void OnBackendAuthorized()
+    {
+        Enqueue(Backend.BMember.GetUserInfo, callback =>
+        {
+            if (!callback.IsSuccess())
+            {
+                Debug.LogError("유저 정보 불러오기 실패\n" + callback);
+                loginSuccessFunc(false, string.Format(BackendError,
+                callback.GetStatusCode(), callback.GetErrorCode(), callback.GetMessage()));
+                return;
+            }
+            Debug.Log("유저정보\n" + callback);
+
+            var info = callback.GetReturnValuetoJSON()["row"];
+            if (info["nickname"] == null)
+            {
+                LoginUI.GetInstance().ActiveNickNameObject();
+                return;
+            }
+            myNickName = info["nickname"].ToString();
+            myIndate = info["inDate"].ToString();
+
+            if (loginSuccessFunc != null)
+            {
+             //   BackEndMatchManager.GetInstance().GetMatchList(loginSuccessFunc);
+                // loginSuccessFunc(true, string.Empty);
+            }
+        });
+    }
+    void Update()
+    {
+        SendQueue.Poll();
+    }
+    public void GuestLogin(Action<bool, string> func)
+    {
+        Enqueue(Backend.BMember.GuestLogin, callback =>
+        {
+            if (callback.IsSuccess())
+            {
+                Debug.Log("게스트 로그인 성공");
+                loginSuccessFunc = func;
+
+                OnPrevBackendAuthorized();
+                return;
+            }
+
+            Debug.Log("게스트 로그인 실패\n" + callback);
+            func(false, string.Format(BackendError,
+                callback.GetStatusCode(), callback.GetErrorCode(), callback.GetMessage()));
+        });
+    }
+
+    //이아래는 친구추가 관련 항목임 알잘딱갈센하샘
+//    public void GetFriendList(Action<bool, List<Friend>> func)
+//    {
+//        Enqueue(Backend.Social.Friend.GetFriendList, 15, callback =>
+//        {
+//            if (callback.IsSuccess() == false)
+//            {
+//                func(false, null);
+//                return;
+//            }
+
+//            var friendList = new List<Friend>();
+
+//            foreach (LitJson.JsonData tmp in callback.Rows())
+//            {
+//                if (tmp.Keys.Contains("nickname") == false)
+//                {
+//                    continue;
+//                }
+//                Friend friend = new Friend();
+//                friend.nickName = tmp["nickname"]["S"].ToString();
+//                friend.inDate = tmp["inDate"]["S"].ToString();
+
+//                friendList.Add(friend);
+//            }
+
+//            func(true, friendList);
+//        });
+//    }
+//    public void GetReceivedRequestFriendList(Action<bool, List<Friend>> func)
+//    {
+//        Enqueue(Backend.Social.Friend.GetReceivedRequestList, 15, callback =>
+//        {
+//            if (callback.IsSuccess() == false)
+//            {
+//                func(false, null);
+//                return;
+//            }
+
+//            var friendList = new List<Friend>();
+
+//            foreach (LitJson.JsonData tmp in callback.Rows())
+//            {
+//                if (tmp.Keys.Contains("nickname") == false)
+//                {
+//                    continue;
+//                }
+//                Friend friend = new Friend();
+//                friend.nickName = tmp["nickname"]["S"].ToString();
+//                friend.inDate = tmp["inDate"]["S"].ToString();
+
+//                friendList.Add(friend);
+//            }
+
+//            func(true, friendList);
+//        });
+//    }
+//    public void RequestFirend(string nickName, Action<bool, string> func)
+//    {
+//        Enqueue(Backend.Social.GetGamerIndateByNickname, nickName, callback =>
+//        {
+//            Debug.Log(callback);
+//            if (callback.IsSuccess() == false)
+//            {
+//                func(false, callback.GetMessage());
+//                return;
+//            }
+//            if (callback.Rows().Count <= 0)
+//            {
+//                func(false, "존재하지 않는 유저입니다.");
+//                return;
+//            }
+//            string inDate = callback.Rows()[0]["inDate"]["S"].ToString();
+//            Enqueue(Backend.Social.Friend.RequestFriend, inDate, callback2 =>
+//            {
+//                Debug.Log(callback2);
+//                if (callback2.IsSuccess() == false)
+//                {
+//                    func(false, callback2.GetMessage());
+//                    return;
+//                }
+
+//                func(true, string.Empty);
+//            });
+//        });
+//    }
+
+//    public void AcceptFriend(string inDate, Action<bool, string> func)
+//    {
+//        Enqueue(Backend.Social.Friend.AcceptFriend, inDate, callback2 =>
+//        {
+//            if (callback2.IsSuccess() == false)
+//            {
+//                func(false, callback2.GetMessage());
+//                return;
+//            }
+
+//            func(true, string.Empty);
+//        });
+//    }
+
+//    public void RejectFriend(string inDate, Action<bool, string> func)
+//    {
+//        Enqueue(Backend.Social.Friend.RejectFriend, inDate, callback2 =>
+//        {
+//            if (callback2.IsSuccess() == false)
+//            {
+//                func(false, callback2.GetMessage());
+//                return;
+//            }
+
+//            func(true, string.Empty);
+//        });
+//    }
+}
+
