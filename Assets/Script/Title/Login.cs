a2b31979c8e0b0f28aff43cdb6302a8789122df2
taewooo0513 <taewooo0513@gmail.com>
2020-11-12 18:23:34 +0900

    로그인

---
+++ Assets/Script/Title/Login.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    public Scene Scene;
+    private string ID,PassWord;
+    public InputField InputID,InputPW;
+    public Button Nextbutton;
+    
+    public void OnClick()
+    {
+        ID = InputID.text;
+        PassWord = InputPW.text;
+        _Login();
+    }
+ 
+    // Start is called before the first frame update
+    void _Login()
+    {
+        Backend.Initialize(() =>
+        {
+            // 초기화 성공한 경우 실행
+            if (Backend.IsInitialized)
+            {
+                Backend.BMember.CustomLogin(ID, PassWord);
+                SceneManager.LoadScene("GameScene");
+
+            }
+            // 초기화 실패한 경우 실행
+            else
+            {
+                _Login();
+            }
+        });
+    }
+    void Register()
+    {
+        Backend.Initialize(() =>
+        {
+            // 초기화 성공한 경우 실행
+            if (Backend.IsInitialized)
+            {
+                Backend.BMember.CustomSignUp(ID, PassWord);
+                SceneManager.LoadScene("GameScene");
+            }
+            // 초기화 실패한 경우 실행
+            else
+            {
+                Register();
+            }
+        });
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+    }
+}
